73f4a1c040c96bf66a15ec91c7d701810f7dd34a
Marwyk2003 <marwyk2003@gmail.com>
2023-06-05 14:20:15 +0200

    Add  banner Fix like button

--- app/app.py
+++ app/app.py
@@ -12,32 +12,57 @@ db = Database()
 USER_ID = None
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    vm = RecipeList(db)
-    vm.load()
-    return render_template('index.html', vm=vm)
+    global USER_ID
+    if request.method == 'GET':
+        vm = RecipeList(db, USER_ID)
+        vm.load()
+        return render_template('index.html', vm=vm)
+    elif request.method == 'POST' and 'btn_login' in request.form:
+        if USER_ID is None:
+            return redirect(url_for('login'))
+        else:
+            USER_ID = None
+            return redirect(url_for('home'))
+    elif request.method == 'POST':
+        return redirect(url_for('home'))
 
 
 @app.route('/recipe/<rid>', methods=['GET', 'POST'])
 def recipe(rid):
+    global USER_ID
     vm = Recipe(db, int(rid), USER_ID)
     vm.load()
-    if request.method == 'POST' and 'btn_like' in request.form:
+    if request.method == 'GET':
+        return render_template('recipe.html', vm=vm, login_msg=login_msg())
+    elif request.method == 'POST' and 'btn_login' in request.form:
+        if USER_ID is None:
+            return redirect(url_for('login'))
+        else:
+            USER_ID = None
+            return redirect(url_for('home'))
+    elif request.method == 'POST' and 'btn_like' in request.form:
         vm.change_like()
+        return redirect(url_for('recipe', rid=rid))
     elif request.method == 'POST' and 'btn_edit' in request.form:
         return redirect(url_for('edit', rid=rid))
-    return render_template('recipe.html', vm=vm)
 
 
 @app.route('/edit/<rid>', methods=['GET', 'POST'])
 def edit(rid):
+    global USER_ID
     if request.method == 'GET':
         vm = Recipe(db, rid, USER_ID)
         vm.load()
-        return render_template('recipe_form.html', vm=vm)
-
-    if request.method == 'POST':
+        return render_template('recipe_form.html', vm=vm, login_msg=login_msg())
+    elif request.method == 'POST' and 'btn_login' in request.form:
+        if USER_ID is None:
+            return redirect(url_for('login'))
+        else:
+            USER_ID = None
+            return redirect(url_for('home'))
+    elif request.method == 'POST':
         vm = RecipeForm(db, int(rid), USER_ID, edit=True)
         vm.name = request.form.get('vm_name')
         vm.difficulty = request.form.get('vm_difficulty')
@@ -55,11 +80,17 @@ def edit(rid):
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
+    global USER_ID
     if request.method == 'GET':
         vm = Recipe(db, None, USER_ID)
-        return render_template('recipe_form.html', vm=vm)
-
-    if request.method == 'POST':
+        return render_template('recipe_form.html', vm=vm, login_msg=login_msg)
+    elif request.method == 'POST' and 'btn_login' in request.form:
+        if USER_ID is None:
+            return redirect(url_for('login'))
+        else:
+            USER_ID = None
+            return redirect(url_for('new'))
+    elif request.method == 'POST':
         vm = RecipeForm(db, None, USER_ID, edit=False)
         vm.name = request.form.get('vm_name')
         vm.difficulty = request.form.get('vm_difficulty')
@@ -77,20 +108,23 @@ def new():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+    global USER_ID
     if request.method == 'GET':
         return render_template('login.html')
-
-    if request.method == 'POST':
+    elif request.method == 'POST':
         login = Login(db)
         username = request.form.get('username')
         password = request.form.get('password')
-        global USER_ID
         USER_ID = login.login(username, password)
         print(f'Now logged in as: {USER_ID}', flush=True)
         if USER_ID is None:
             return redirect(url_for('login'))
         USER_ID = int(USER_ID)
-        return redirect('/')
+        return redirect(url_for('home'))
+
+
+def login_msg():
+    return 'Log in' if USER_ID is None else 'Log out'
 
 
 if __name__ == '__main__':
--- app/viewmodels/RecipeList.py
+++ app/viewmodels/RecipeList.py
@@ -1,10 +1,13 @@
 class RecipeList:
-    def __init__(self, db):
+    def __init__(self, db, user_id):
         self.recipes = []
+        self.user_id = user_id
+        self.login_msg = 'Log in'
 
         self.db = db
 
     def load(self):
+        self.login_msg = 'Log in' if self.user_id is None else 'Log out'
         query = self.db.exec(
             '''
                 SELECT * FROM top_recipes
